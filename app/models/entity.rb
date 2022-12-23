1830bb4405e9fe4cfbc70785216fe2e08cc90420
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-12-23 09:52:27 +0200

    Remove Confimable

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,6 +1,5 @@
 class Entity < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  # has_and_belongs_to_many :groups
   has_many :group_entities
 
   validates :name, presence: true
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  # has_and_belongs_to_many :entities
   has_many :group_entities
 
   validates :name, presence: true
